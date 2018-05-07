41597872a8d4d7a019d3057bf3e026f5d7a13b4c
Michael <torrestmichael@gmail.com>
2018-05-07 16:27:19 -0400

    Everything
    Everything

---
+++ exam4.js
@@ -0,0 +1,23 @@
+var app4 = angular.module('app4', []);
+
+app4.controller('eventCtrl', function($scope) {
+    
+    $scope.dayTimeButton = true;
+    
+    $scope.capitals = [
+        
+        {"City": "Montgomery",
+        "State": "Alabama"},
+        
+        {"City": "Juneau",
+        "State": "Alaska"},
+        
+        {"City": "Phoenix",
+        "State": "Arizona"},
+        
+        {"City": "Little Rock",
+        "State": "Arkansas"},
+        
+    ];
+    
+});
